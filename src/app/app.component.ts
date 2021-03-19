01648dd23655d48980f1e28f5358e86d31354c2b
Vaishnavi <vaishnavisdesai@gmail.com>
2021-03-19 23:43:42 +0530

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IeeeFestSiliconValley';
+
+  ngOnInit(){
+  }
+
+  toTrailer() {
+    document.getElementById("trailer").scrollIntoView({ block: 'end', behavior: 'smooth' });
+  }
+
+  toMovieWebsite() {
+    document.getElementById("moviewebsite").scrollIntoView({ block: 'end', behavior: 'smooth' });
+  }
+
+  toSynopsis(){
+    document.getElementById("synopsis").scrollIntoView({ block: 'end', behavior: 'smooth' });
+  }
+
+  toCast(){
+    document.getElementById("cast").scrollIntoView({ block: 'end', behavior: 'smooth' });
+  }
+
+  toOst(){
+    document.getElementById("ost").scrollIntoView({ block: 'end', behavior: 'smooth' });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
